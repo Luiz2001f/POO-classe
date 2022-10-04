a8b8677ec0eff5839545cdd5917e1446ff95329e
Luiz2001f <luiz-granata@hotmail.com>
2022-10-03 22:04:07 -0300

    alexa toca funk pesadao

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,8 +2,9 @@ public class Pessoa {
     private float saldo;
 
     public void somaSaldo(float valor) {
-        this.saldo +=valor;
+        this.saldo += valor;
     }
+
     public float pegaSaldo() {
         return this.saldo;
     }
